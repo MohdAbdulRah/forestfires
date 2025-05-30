a7ee80189267cbe8d00ac1cc7e0e6fae217c4a30
MohdAbdulRah <mohdnawaz5421@gmail.com>
2025-05-30 13:06:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def predict_datapoint():
         return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))  # use the PORT environment variable
+    app.run(host='0.0.0.0', port=port)
